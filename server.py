09c505cdf430d2d11fb03e6720fae3cf73ef34b5
pakkawat <pakkawat.jong@gmail.com>
2015-02-13 15:40:48 +0700

    t2

--- server.py
+++ server.py
@@ -2,24 +2,6 @@
 
 import socket               # Import socket module
 
-s = socket.socket()         # Create a socket object
-host = socket.gethostname() # Get local machine name
-port = 80                # Reserve a port for your service.
-s.bind((host, port))        # Bind to the port
-
-s.listen(5)                 # Now wait for client connection.
-while True:
-   c, addr = s.accept()     # Establish connection with client.
-   print 'Got connection from', addr
-
-   data = c.recv(1024)
-   print check_request(data)
-   c.send("""HTTP/1.0 200 OK
-Content-Type:text/html
-
-<h1>Hello World</h1>""")
-   c.close()                # Close the connection
-
 def check_request(data):
   if "GET" in data:
     data = find_part_tp_file(data)
@@ -39,5 +21,24 @@ def find_part_to_file(data):
   else: #DELETE
     temp_start = 7
 
-  data = [temp_start:temp_end]
+  data = data[temp_start:temp_end]
   return data
+
+
+s = socket.socket()         # Create a socket object
+host = socket.gethostname() # Get local machine name
+port = 80                # Reserve a port for your service.
+s.bind((host, port))        # Bind to the port
+
+s.listen(5)                 # Now wait for client connection.
+while True:
+   c, addr = s.accept()     # Establish connection with client.
+   print 'Got connection from', addr
+
+   data = c.recv(1024)
+   print check_request(data)
+   c.send("""HTTP/1.0 200 OK
+Content-Type:text/html
+
+<h1>Hello World</h1>""")
+   c.close()                # Close the connection
