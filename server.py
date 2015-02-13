edbdd6c006a4302455c3441627d28bcff73fe3ac
pakkawat <pakkawat.jong@gmail.com>
2015-02-13 18:23:10 +0700

    t7

--- server.py
+++ server.py
@@ -3,13 +3,27 @@
 import socket               # Import socket module
 import os
 
+def create_html_list(data):
+  temp = """HTTP/1.0 200 OK
+Content-Type:text/html
+
+<h1>Directory listing for / </h1><hr><ul>"""
+  for f in data:
+    temp += "<li>" + f + "</li>"
+
+  temp += "</ul><hr>"
+  return temp
+
+
 def get_file(data):
-  temp = ""
-  if len(data) == 1:
+  temp = []
+  if len(data) == 2:
     files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
     for f in files:
       if f[0] != ".":
-        temp = temp + f + "--"
+        temp += f
+
+    create_html_list(temp)
 
   return temp
 def part_to_file(data):
@@ -47,9 +61,5 @@ while True:
    print 'Got connection from', addr
 
    data = c.recv(1024)
-   print check_request(data)
-   c.send("""HTTP/1.0 200 OK
-Content-Type:text/html
-
-<h1>Hello World</h1>""")
+   c.send(check_request(data))
    c.close()                # Close the connection
