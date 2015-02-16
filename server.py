7f32988c03dc6e823e56ff2e3a1a588db95ce0b6
pakkawat <pakkawat.jong@gmail.com>
2015-02-16 14:10:16 +0700

    t15

--- server.py
+++ server.py
@@ -10,7 +10,9 @@ def response_file(data,port):
 Content-Type:text/html
 
 <h1>file </h1><hr>"""
-  temp += data
+  m = magic.open(magic.MAGIC_MIME_TYPE)
+  m.load()
+  temp += m.file(data[0:len(data)-1])
   return temp
 
 def response_html_list(data,port):
@@ -20,45 +22,29 @@ Content-Type:text/html
 <h1>Directory listing for / </h1><hr><ul>"""
   for f in data:
     if f[0] != ".":
-      temp += "<li><a href='http://" + socket.gethostname() + ".cloudapp.net:" + port + "/" + f + "'>" + f + "</a></li>"
+      temp += "<li><a href='http://" + socket.gethostname() + ".cloudapp.net:" + str(port) + "/" + f + "'>" + f + "</a></li>"
 
   temp += "</ul><hr>"
   return temp
 
 
-def get_file(data,port):
+def get_file(data):
   if len(data) == 2:
     files = [f for f in os.listdir(os.getcwd()) if os.path.isfile(f)]
-    response_html_list(files,port)
+    return response_html_list(files,port)
   else:
     data = data[1:]
     return response_file(data,port)
 
 
 
-def part_to_file(data,port):
-  temp_start = 0
-  temp_end = data.find("HTTP")
-
-  if "GET" in data:
-    temp_start = 4
-    data = data[temp_start:temp_end]
-    data = get_file(data,port)
-  else: #DELETE
-    temp_start = 7
-
+def part_to_file(data,start):
+  end = data.find("HTTP")
+  data = data[start:end]
 
   return data
 
-def check_request(data,port):
-  if "GET" in data:
-    data = part_to_file(data,port)
-  elif "DELETE" in data:
-    data = part_to_file(data,port)
-  elif "HEAD" in data:
-    print "HEAD"
 
-  return data
 #------------------------------------------------------------
 s = socket.socket()         # Create a socket object
 host = socket.gethostname() # Get local machine name
@@ -71,5 +57,15 @@ while True:
    print 'Got connection from', addr
 
    data = c.recv(1024)
-   c.send(check_request(data,port))
+   if not "favicon.ico" in data:
+     if "GET" in data:
+       path = part_to_file(data,4)
+     elif "DELETE" in data:
+       path = part_to_file(data,7)
+     elif "HEAD" in data:
+       print "HEAD"
+
+     files = get_file(path)
+     print files
+     #c.send(check_request(data,port))
    c.close()                # Close the connection
